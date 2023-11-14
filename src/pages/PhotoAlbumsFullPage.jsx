1c1ea13c681baa918664f8d36f7fde67ae89f2d6
Oleksandra Tkachuk <s.tkachuk@wishdesk.com>
2023-11-14 18:01:44 +0200

    ST-#789606:Displayed the photo gallery on single pages, added the LightBox package

--- src/pages/PhotoAlbumsFullPage.jsx
+++ src/pages/PhotoAlbumsFullPage.jsx
@@ -2,32 +2,35 @@ import {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import useDrupalData from "../services/api.jsx";
 import ImageComponent from "../components/ImageComponent.jsx";
-import LightBoxComponent from "../components/LightBoxComponent.jsx";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 function PhotoAlbumsFullPage() {
     const navigate = useNavigate();
     const {albumsalias} = useParams();
     const [apiUrl, setApiUrl] = useState("/jsonapi/views/photoalbums_/block_1");
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
     const {
         data: albumsNode,
         isLoading: albumsNodeIsLoading,
         error: albumsNodeError,
     } = useDrupalData(`photoalbums/${albumsalias}?_format=json`);
-
     return (
-        <div className="albums">
+        <div className="albums-container">
             {albumsNode.title?.map((item, index) => (
-                <div className="w-full" key={index}>
+                <div className="album-title" key={index}>
                     <h1>{item.value}</h1>
                 </div>
             ))}
+            <div className="album-photo">
             {albumsNode.field_photos?.map((item, index) => (
-                <div key={index}>
-                    <ImageComponent url={`jsonapi/media/${item.target_id}/edit?_format=json`} /><div>
-                </div>
-
-                </div>
+                    <ImageComponent url={`jsonapi/media/image/${item.target_uuid}/field_media_image`} imagestyle={"photoalbums_"} />
             ))}
+            </div>
+            {/*MAIN LIGHTBOX*/}
+
 
         </div>
     );
