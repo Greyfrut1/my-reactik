e3586292289b81548a55fe57f027f19122ba54b9
Greyfrut1 <vladchumachenko496@gmail.com>
2023-11-14 18:13:22 +0200

    Add Main Slider, Comming soon events, start work on poll block

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,106 @@
-import {useEffect, useState} from 'react'
 import useDrupalData from "../../services/api.jsx";
 import ImageComponent from "../../components/ImageComponent.jsx";
+import { format } from 'date-fns';
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
+import {useState, useEffect} from 'react'
+
+function truncateText(text, maxLength) {
+    if (text && text.length > maxLength) {
+        return text.slice(0, maxLength) + '...';
+    }
+    return text;
+}
 
 function Home() {
+    const [showForm1, setShowForm1] = useState(true);
+    const handleButtonClick = () => {
+        setShowForm1(!showForm1); // Перемикач форм
+    };
     const {data: data, isLoading: isLoading, error: error} = useDrupalData('jsonapi/views/actual_news/block_1');
     const {data: data2, isLoading: isLoading2, error: error2} = useDrupalData('jsonapi/views/last_news/block_1');
+    const {data: data3, isLoading: isLoading3, error: error3} = useDrupalData('jsonapi/views/events_coming_soon/block_1');
+    const {data: data4, isLoading: isLoading4, error: error4} = useDrupalData('jsonapi/views/slider/block_1');
+    const {data: data5, isLoading: isLoading5, error: error5} = useDrupalData('/jsonapi/views/polls/block_1');
+    const {data: data6, isLoading: isLoadin6, error: error6} = useDrupalData('/poll-vote-result/rest-export/1');
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+    };
+
+    const [ip, setIP] = useState("");
+
+    //creating function to load ip address from the API
+    // const getData = async () => {
+    //   const res = await axios.get("https://geolocation-db.com/json/");
+    //   console.log(res.data);
+    //   setIP(res.data.IPv4);
+    // };
+
+    // Updated Code
+
+    const getData = async () => {
+        const res = await axios.get("https://api.ipify.org/?format=json");
+        console.log(res.data);
+        setIP(res.data.ip);
+    };
+
+    useEffect(() => {
+        //passing getData method to the lifecycle method
+        getData();
+    }, []);
+
+    const handleSubmit1 = (event) => {
+
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        const formData = new FormData(event.target);
+        const selectedValue = formData.get('choice');
+        const pollId = formData.get('pollId');
+        const currentTimestamp = new Date().getTime();
+        const currentHostname = window.location.hostname;
+
+
+        console.log('Poll ID:', pollId);
+        console.log('Current Timestamp:', currentTimestamp);
+        console.log('Current Hostname:', currentHostname);
+
+
+        const submitFormData = {
+            chid: selectedValue,
+            pid: pollId,
+            uid: "0",
+            hostname: ip,
+            timestamp: Math.floor(currentTimestamp / 1000).toString(),
+        };
+        console.log(submitFormData)
+
+        axios.post('http://128.140.43.32/poll-vote/post-data', submitFormData)
+            .then((response) => {
+                // Handle the server response
+                console.log(response.data, response);
+            })
+            .catch((error) => {
+                // Handle errors
+                console.error(error);
+            });
+
+        // Your custom logic goes here
+        console.log('test', event.target);
+
+        // Optionally, you can use the FormData API to access form data
+         // 'choice' corresponds to the name attribute of the radio inputs
+
+        console.log('Selected value:', selectedValue);
+        console.log('Form data:', formData);
+    };
 
     // useEffect(() => {
     //     if (!isLoading && !isLoading2) {
@@ -23,6 +119,16 @@ function Home() {
 
     return (
         <div>
+            <Slider {...settings}>
+                {data4?.data?.map((slide) => (
+                    <div key='test'>
+                        <h3>{slide?.attributes?.info}</h3>
+                        <ImageComponent url={`jsonapi/block_content/slider/${slide?.id}/field_image`} imageStyle={'news_275x185'}/>
+                        <div dangerouslySetInnerHTML={{ __html: slide?.attributes?.field_description?.value }} />
+                        <a href='/'>Докладніше</a>
+                    </div>
+                ))}
+            </Slider>
             <div>
                     {data?.data?.map((article) => (
 
@@ -48,6 +154,51 @@ function Home() {
                         </div>
                     ))}
                 </div>
+            </div>
+            <div>
+                <h2><a href='/'>Незабаром</a></h2>
+                {data3?.data?.map((event) => (
+                    <div key={event.id}>
+                        <div><div>{format(new Date(event?.attributes?.field_duration?.value), 'dd MMMM HH:mm')}</div>
+                            <div>{format(new Date(event?.attributes?.field_duration?.end_value), 'dd MMMM HH:mm')}</div>
+                        </div>
+                        <ImageComponent url={`jsonapi/node/events/${event?.id}/field_image`} imageStyle={'news_275x185'}/>
+                        <div><h3>{event?.attributes?.title}</h3></div>
+                        <div dangerouslySetInnerHTML={{ __html: truncateText(event?.attributes?.field_description?.value, 150) }} />
+                        <div><a href='/'><img src="/src/assets/long-arrow-right.png" alt='link'/></a></div>
+                    </div>
+                ))}
+            </div>
+            <div className='homepage-bottom'>
+                {data5?.data?.map((poll) => (
+                    <div key='test'>
+                        <button onClick={handleButtonClick}>Перемикати форми</button>
+                        {showForm1 ? (
+                        <form onSubmit={handleSubmit1}>
+                            <input type="hidden" name="pollId" value={poll?.attributes?.drupal_internal__id} />
+                            {poll?.relationships?.choice?.data?.map((choice) => (
+                                <div key={choice.id}>
+                                    <input
+                                        type="radio"
+                                        name="choice"  // Add a name attribute to group the radio inputs
+                                        value={choice?.meta?.drupal_internal__target_id}
+                                    />
+                                    <label>{choice?.meta?.drupal_internal__target_id}</label>
+                                </div>
+                            ))}
+                            <button type="submit">Надіслати</button>
+                        </form>
+                        ) : (
+                            <div>
+                            {data6?.data?.map((choice) => (
+                                <div key={choice.id}>
+                                    {choice?.attributes?.vote_count}
+                                </div>
+                            ))}
+                            </div>
+                        )}
+                    </div>
+                ))}
             </div>
         </div>
     );
