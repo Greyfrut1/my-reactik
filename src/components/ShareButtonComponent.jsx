248ccbae25902fbab1c3b2612e6cb51ac00e29cc
Oleksandra Tkachuk <s.tkachuk@wishdesk.com>
2023-11-22 12:23:35 +0200

    ST-#792386:Added Telegram share buttons

--- src/components/ShareButtonComponent.jsx
+++ src/components/ShareButtonComponent.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from 'react-share';
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, TelegramIcon, TelegramShareButton } from 'react-share';
 
 const ShareButtonComponent = ({ data }) => {
-    const shareUrl = `http://128.140.43.32${data.path?.[0]?.alias}`;
-    const title = data.title?.[0]?.value || '';
+    const shareUrl = `${window.location.origin}${data.path?.[0]?.alias}`;
+    const title = data.title?.[0]?.value;
 
     return (
-        <div>
+        <div className={'share-link'}>
             <FacebookShareButton url={shareUrl} quote={title}>
-                <FacebookIcon size={32} round />
+                <FacebookIcon size={35} round />
             </FacebookShareButton>
             <TwitterShareButton url={shareUrl} title={title}>
-                <TwitterIcon size={32} round />
+                <TwitterIcon size={35} round />
             </TwitterShareButton>
+            <TelegramShareButton url={shareUrl} title={title}>
+                <TelegramIcon size={35} round />
+            </TelegramShareButton>
         </div>
     );
 };
