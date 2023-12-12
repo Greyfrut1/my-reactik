4f16d52ee80d0d6f4a351ab5bf127592d4324185
Greyfrut <greyfrut@wishdesk.com>
2023-12-12 17:31:36 +0200

    ST-#808543: Create OnlineVisitors component. Create server for count online visitors site. Add script server to package json

---
+++ server/server.cjs
@@ -0,0 +1,36 @@
+// server/server.cjs
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const domain = process.env.VITE_FRONTEND_URL
+
+
+const app = express();
+app.use(cors({ origin: domain }));
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: domain,
+        methods: ['GET', 'POST']
+    }
+});
+
+let onlineUsers = 0;
+
+io.on('connection', (socket) => {
+    onlineUsers++;
+    io.emit('updateUsers', onlineUsers);
+
+    socket.on('disconnect', () => {
+        onlineUsers--;
+        io.emit('updateUsers', onlineUsers);
+    });
+});
+
+const PORT = process.env.VITE_SERVER_PORT || 5178;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
