71357a651088ad76810d9678883c40d57cacf339
Greyfrut <greyfrut@wishdesk.com>
2023-12-29 11:26:49 +0200

    tset

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "../styles/css/styles.css"
 import {BrowserRouter} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
-          <App />
+          <Helmet>
+              <title>My Title</title>
+              <meta name="description" content="Helmet application"/>
+          </Helmet>
+          <App/>
       </BrowserRouter>
   </React.StrictMode>,
 )
