453608099b162082f021b2f39d2e35045f585c0e
Greyfrut <greyfrut@wishdesk.com>
2023-12-29 12:16:04 +0200

    asdsad

--- src/main.jsx
+++ src/main.jsx
@@ -4,27 +4,31 @@ import App from './App.jsx'
 import "../styles/css/styles.css"
 import {BrowserRouter} from "react-router-dom";
 import {Helmet} from "react-helmet";
-
+import { HelmetProvider } from 'react-helmet-async';
+const helmetContext = {};
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Helmet>
-          <title>My Title</title>
-          <meta name="description" content="Helmet application"/>
-          <meta property="og:url" content="https://my-reactik.vercel.app/en"/>
-          <meta property="og:type" content="website"/>
-          <meta property="og:title" content="VNtsetesttestU"/>
-          <meta property="og:description" content="123123"/>
-          <meta property="og:image" content=""/>
+    <HelmetProvider context={helmetContext}>
+        <Helmet>
+            <title>My Title</title>
+            <meta name="description" content="Helmet application"/>
+            <meta property="og:url" content="https://my-reactik.vercel.app/en"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="VNtsetesttestU"/>
+            <meta property="og:description" content="123123"/>
+            <meta property="og:image" content=""/>
 
-          <meta name="twitter:card" content="summary_large_image"/>
-          <meta property="twitter:domain" content="my-reactik.vercel.app"/>
-          <meta property="twitter:url" content="https://my-reactik.vercel.app/en"/>
-          <meta name="twitter:title" content="123123123"/>
-          <meta name="twitter:description" content="123213"/>
-          <meta name="twitter:image" content=""/>
-      </Helmet>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:domain" content="my-reactik.vercel.app"/>
+            <meta property="twitter:url" content="https://my-reactik.vercel.app/en"/>
+            <meta name="twitter:title" content="123123123"/>
+            <meta name="twitter:description" content="123213"/>
+            <meta name="twitter:image" content=""/>
+        </Helmet>
+  <React.StrictMode>
+
       <BrowserRouter>
           <App/>
       </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+    </HelmetProvider>
 )
