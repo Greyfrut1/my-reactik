de4ca3eda8e4c501b1510cb712aa20a6f9b8f1f2
Greyfrut <greyfrut@wishdesk.com>
2023-12-29 12:10:50 +0200

    testest

--- src/main.jsx
+++ src/main.jsx
@@ -7,23 +7,23 @@ import {Helmet} from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Helmet>
-              <title>My Title</title>
-              <meta name="description" content="Helmet application"/>
-              <meta property="og:url" content="https://my-reactik.vercel.app/en"/>
-              <meta property="og:type" content="website"/>
-              <meta property="og:title" content="VNtsetesttestU"/>
-              <meta property="og:description" content="123123"/>
-              <meta property="og:image" content=""/>
+      <Helmet>
+          <title>My Title</title>
+          <meta name="description" content="Helmet application"/>
+          <meta property="og:url" content="https://my-reactik.vercel.app/en"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="VNtsetesttestU"/>
+          <meta property="og:description" content="123123"/>
+          <meta property="og:image" content=""/>
 
-              <meta name="twitter:card" content="summary_large_image"/>
-              <meta property="twitter:domain" content="my-reactik.vercel.app"/>
-              <meta property="twitter:url" content="https://my-reactik.vercel.app/en"/>
-              <meta name="twitter:title" content="123123123"/>
-              <meta name="twitter:description" content="123213"/>
-              <meta name="twitter:image" content=""/>
-          </Helmet>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:domain" content="my-reactik.vercel.app"/>
+          <meta property="twitter:url" content="https://my-reactik.vercel.app/en"/>
+          <meta name="twitter:title" content="123123123"/>
+          <meta name="twitter:description" content="123213"/>
+          <meta name="twitter:image" content=""/>
+      </Helmet>
+      <BrowserRouter>
           <App/>
       </BrowserRouter>
   </React.StrictMode>,
