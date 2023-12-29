26f7f3eb6d56b0a7a7d9b98cbac362729fa3edec
Greyfrut <greyfrut@wishdesk.com>
2023-12-29 11:29:04 +0200

    gsdgds

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Helmet>
               <title>My Title</title>
               <meta name="description" content="Helmet application"/>
+              <meta property="og:title" content="How to Become an SEO Expert (8 Steps)"/>
+              <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps."/>
+              <meta property="og:image"
+                    content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png"/>
           </Helmet>
           <App/>
       </BrowserRouter>
