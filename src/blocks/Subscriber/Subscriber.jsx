9200fd7ad6a5dc978d8d43a0ebe8b7789f1de239
Oleksandra Tkachuk <s.tkachuk@wishdesk.com>
2024-01-16 17:08:23 +0200

    ST-#827577:Add stylization for popup

--- src/blocks/Subscriber/Subscriber.jsx
+++ src/blocks/Subscriber/Subscriber.jsx
@@ -1,17 +1,16 @@
 import axios from "axios";
 import {useState} from "react";
-import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {useNewsLetterSubscriberQuery} from "../../services/api.js";
 import useLanguagePrefix from "../../services/languagePrefix.jsx";
-import SocialLinks from "../../components/SocialLinks.jsx";
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 import './Subscriber.scss';
 
 export default function Subscriber() {
     const [email, setEmail] = useState('');
+    const {data} = useNewsLetterSubscriberQuery();
+    const img = data?.data?.field_image?.image_style_uri?.['news_440x232'];
 
-    const {data: newsletterBlock} = useNewsLetterSubscriberQuery();
     const handleSubmit = (event) => {
 
         event.preventDefault();
@@ -38,9 +37,26 @@ export default function Subscriber() {
 
     return (
         <div className="subscriber">
-            <form onSubmit={handleSubmit}>
+            <h1 className="subscriber-title">
+                {data?.data.info}
+            </h1>
+            <p
+                className="subscriber-text"
+                dangerouslySetInnerHTML={{
+                    __html: data?.data.body?.processed,
+                }}
+            />
+            <form onSubmit={handleSubmit} className={"flex w-1/2 relative items-center"}>
+                <input required={"required"} type={"email"} value={email}
+                       onChange={(e) => setEmail(e.target.value)}
+                       placeholder={(langPrefix === "en" && "Enter your e-mail") || (langPrefix === "uk" && "Введіть e-mail")}/>
                 <button
-                    type="submit">{(langPrefix === "en" && "Subscribe") || (langPrefix === "uk" && "Підписатися")}</button>
+                    type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                         stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
+                    </svg>
+                </button>
             </form>
         </div>
     );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,12 @@
 import PropTypes from 'prop-types';
 import Header from '../blocks/Header/Header.jsx';
 import Footer from "../blocks/Footer/Footer.jsx";
-import {ToastContainer} from "react-toastify";
 
 function Layout({ children }) {
     return (
         <>
            <Header/>
             <main>{children}</main>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored"
-            />
             <Footer />
         </>
     );
